f61a438b99ce3d50ac841b3164568d0d3dc4a6cd
allan <allansaleh95@gmail.com>
2025-01-15 06:53:02 -0500

    Getting Started with React

---
+++ Getting Started with React/app/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello everyone!</h1>
+    </div>
+  )
+}
+export default App
---
+++ Getting Started with React/app/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    const name = "Allan"
+  return (
+    <div>This is the Navbar {name}</div>
+  )
+}
+export default Navbar
